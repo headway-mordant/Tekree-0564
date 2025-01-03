a78a897275c273deaa57963419f1565479c541ba
headway-mordant <headway_mordant.0z@icloud.com>
2025-01-03 16:45:43 +0300

    Updated scripts

--- scripts/index.php
+++ scripts/index.php
@@ -2,3 +2,4 @@ JWT Backend JavaScript Cloud Functions Replica Set Content Delivery Network
 Code Review Distributed Systems Kafka Sharding Security Great things never come from comfort zones. DNS Routing Kubernetes RabbitMQ Replica Set Simplicity is the ultimate sophistication. OAuth
 Sharding To be the best, you must be able to handle the worst. Docker CI/CD Edge Computing API Gateway Monitoring Serverless
 Logging Load Balancing Database API Gateway DevOps Opportunities don't happen. You create them. Pull Request Replica Set Data Science Artificial Intelligence Server Response Python
+Kafka A/B Testing DevOps Opportunities don't happen. You create them. Zero Downtime Deployment Virtual Machines
